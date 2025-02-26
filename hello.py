2a54c0a94ad8655c171918cb2bd65d23f353c05d
Ali Tavallaie <a.tavallaie@gmail.com>
2025-02-26 19:28:08 +0330

    ignore python.org, it is finished

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
 from safarnama import load_config, SiteCrawler
 
-# Crawl python.org
-config_python = load_config("config_python.yaml")
-crawler_python = SiteCrawler(config_python)
-visited_python = crawler_python.crawl()
-print(f"Crawled {len(visited_python)} URLs from python.org.")
+# # Crawl python.org
+# config_python = load_config("config_python.yaml")
+# crawler_python = SiteCrawler(config_python)
+# visited_python = crawler_python.crawl()
+# print(f"Crawled {len(visited_python)} URLs from python.org.")
 
-# Optionally, generate a sitemap for python.org
-sitemap_python = crawler_python.generate_sitemap(visited_python)
-sitemap_python.write("data/sitemap_python.xml", encoding="utf-8", xml_declaration=True)
-crawler_python.close()
+# # Optionally, generate a sitemap for python.org
+# sitemap_python = crawler_python.generate_sitemap(visited_python)
+# sitemap_python.write("data/sitemap_python.xml", encoding="utf-8", xml_declaration=True)
+# crawler_python.close()
 
 # Crawl wiki.python.org
 config_wiki = load_config("config_wiki.yaml")
